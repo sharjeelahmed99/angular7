20b9487af66b4bc5dca48e2f23770a8ef88e970d
LAPTOP-AVIZIA-2\Administrator <kashif.atiq@tenpearls.com>
2019-03-17 17:17:04 +0500

    search complete

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../modals/hero';
 import { HeroService } from '../services/hero.service';
+import { Subject, Observable } from 'rxjs';
+import { debounce, distinctUntilChanged, switchMap, debounceTime, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
@@ -10,10 +12,19 @@ import { HeroService } from '../services/hero.service';
 export class HeroSearchComponent implements OnInit {
 
   constructor(private heroService: HeroService) { }
-
+  private searchTerm =new Subject<string>();
+  heroes: Hero[];
   ngOnInit() {
+    this.searchTerm.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term:string)=> this.heroService.search(term).pipe(
+        tap((res)=> console.log(res) )
+      )),
+      
+    ).subscribe((res)=>this.heroes =res)
   }
   search(term:string): void{
-
+this.searchTerm.next(term);
   }
 }
